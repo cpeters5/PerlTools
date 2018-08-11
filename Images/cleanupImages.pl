d981341e535a72374d86058a39481ca2367ca391
Chariya <cpeters5@yahoo.com>
2018-08-11 19:59:52 -0400

    Get rid of command line argument. Run twice in a loop species and hybrid.

--- Images/cleanupImages.pl
+++ Images/cleanupImages.pl
@@ -14,63 +14,55 @@ use DBD::ODBC;
 
 # require "common.pl";
 my ($sth);
-my (%img,%src,%nam,%fnm,%genus,%gen);
-my $type = $ARGV[0];
-my $imgtype;
 my $stmt;
-my $tab;
-my $mediaroot = 'C:/projects/orchids/git/static/utils/images/';
- $mediaroot = '/home/chariya/webapps/static_media/utils/images/';
+my $mediaroot = 'C:/projects/orchids/orchidproject/static/utils/images/';
+   # $mediaroot = '/home/chariya/webapps/static_media/utils/images/';
 my $discard_file = $mediaroot."discard_file/";
 my $discard_thmb = $mediaroot."discard_thmb/";
-my ($image_dir, $thumb_dir);
 
 system( 'mkdir '.$discard_file ) if ( ! -d $discard_file );
 system( 'mkdir '.$discard_thmb ) if ( ! -d $discard_thmb );
-
-if ($ARGV[0] eq 'species') {
-    $tab = "orchid_spcimages";
-    $image_dir = $mediaroot . 'species/';
-    $thumb_dir = $mediaroot . 'species_thumb/';
-    $type = "spc";
-
-}
-elsif ($ARGV[0] eq 'hybrid') {
-    $tab = "orchid_hybimages";
-    $image_dir = $mediaroot . 'hybrid/';
-    $thumb_dir = $mediaroot . 'hybrid_thumb/';
-    $type = "hyb";
-}
 
 # Database connection
 my $dbh = DBI->connect( "DBI:ODBC:Bluenanta") or die( "Could not connect to: $DBI::errstr" );
 my $DB = "orchidroots";
 &getASPM("use $DB");
 
-
-print "Initialize images\n";
+my @type = ("species","hybrid");
+my %type = (species=>"spc", hybrid=>"hyb");
+my %tab  = (species=>"orchid_spcimages", hybrid=>"orchid_hybimages");
 my %files = ();
-getImages($tab);
 
-# Process files
-print "Process image objects\n";
-processFiles();
+foreach my $type (@type) {
+	print "Initialize images\n";
+	%files = ();
+	getImages($tab{$type});
+
+	# Process files
+	print "Process image objects\n";
+	processFiles($type);
+
+}
 
 sub processFiles {
+	my $type = shift;
+	my $image_dir = $mediaroot . $type . '/';
+	my $thumb_dir = $mediaroot . $type . '_thumb/';
+
     opendir(my $dh, $image_dir) or die "cant open $image_dir : $!\n";
+	print "image dir = $image_dir\n";
     my $i = 0;
     my @filelist = readdir $dh;
 	foreach (@filelist) {
 	    next if exists $files{$_};
-	    next if $_ !~ /^$type/;
+	    next if $_ !~ /^$type{$type}/;
 		print $i++."\t$_\n";
 		my $from = $image_dir .  $_;
 		move $from, $discard_file;
 		my $from_tmb = $thumb_dir  . $_;
 		move $from_tmb, $discard_thmb;
-#        print "move $from_tmb\tto\t$discard_thmb\n"; sleep 1;
 	}
-	print "\n\tMoved $i $ARGV[0] image files to discard folder\n";
+	print "\n\tMoved $i $type image files to discard folder\n";
 }
 
 sub getImages {
@@ -83,7 +75,6 @@ sub getImages {
    
 	while (my @row = $sth->fetchrow_array()) {
     	if ($row[0]) {
-#	    	print "$row[0]\n"; sleep 1;
 		    $files{$row[0]}++;
 		}
 	}
